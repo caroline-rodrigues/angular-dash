97666dd074dbe62c9f8b2e521340fcacb5f0d4e6
Vitor Pantoja <vitortravassos3@gmail.com>
2023-08-09 02:04:08 -0300

    WIP
    Construindo tela de criar e editar rent.

--- src/app/components/new-rent/new-rent.component.ts
+++ src/app/components/new-rent/new-rent.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { ClientService } from "app/client/client.service";
 import { VehicleService } from "app/vehicle/vehicle.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { map } from "rxjs/operators";
 
 declare interface User {
   text?: string;
-  email?: string; //  must be valid email format
-  password?: string; // required, value must be equal to confirm password.
-  confirmPassword?: string; // required, value must be equal to password.
-  number?: number; // required, value must be equal to password.
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  number?: number;
   url?: string;
   idSource?: string;
   idDestination?: string;
@@ -22,22 +23,69 @@ export class NewRentComponent implements OnInit {
   public typeValidation: User;
   clientList: any[] = [];
   vehicleList: any[] = [];
+  rentForm: FormGroup;
+  selectedClient: any;
 
   constructor(
     private clientService: ClientService,
-    private vehicleService: VehicleService
+    private vehicleService: VehicleService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
-    this.typeValidation = {
-      text: "",
-      email: "",
-      idSource: "",
-      idDestination: "",
-      url: "",
-    };
+    this.createRentForm();
     this.findAllClients();
     this.findAllVehicles();
+    // this.rentForm = this.formBuilder.group({
+    //   cpf: [""],
+    //   plate: [""],
+    //   email: [""],
+    //   idSource: [""],
+    //   idDestination: [""],
+    //   url: [""],
+    // });
+  }
+
+  createRentForm() {
+    this.rentForm = this.formBuilder.group({
+      client: [""],
+      cpf: ["", Validators.required],
+      plate: ["", Validators.required],
+      vehicle: [""],
+      email: ["", Validators.email],
+      idSource: [""],
+      idDestination: [""],
+      url: [""],
+      // ... other form fields ...
+    });
+
+    this.rentForm.get("client").valueChanges.subscribe((selectedClient) => {
+      console.log(selectedClient);
+      if (selectedClient && selectedClient.cpf) {
+        this.rentForm
+          .get("cpf")
+          .setValue(selectedClient.cpf, { emitEvent: false });
+      } else {
+        this.rentForm.get("cpf").setValue("", { emitEvent: false });
+      }
+    });
+    this.rentForm.get("vehicle").valueChanges.subscribe((selectedVehicle) => {
+      console.log(selectedVehicle);
+      if (selectedVehicle && selectedVehicle.plate) {
+        this.rentForm
+          .get("plate")
+          .setValue(selectedVehicle.plate, { emitEvent: false });
+      } else {
+        this.rentForm.get("plate").setValue("", { emitEvent: false });
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.rentForm.valid) {
+      // Do something with the form data
+      console.log(this.rentForm.value);
+    }
   }
 
   findAllClients() {
@@ -46,6 +94,7 @@ export class NewRentComponent implements OnInit {
       .pipe(map((clientResponse) => clientResponse.results))
       .subscribe((client) => {
         this.clientList = client;
+        console.log(this.clientList);
       });
   }
   findAllVehicles() {
@@ -56,5 +105,10 @@ export class NewRentComponent implements OnInit {
         this.vehicleList = vehicle;
         console.log(vehicle);
       });
+  }
+
+  onClientSelect(valorSelecionado: any) {
+    console.log({ valorSelecionado });
+    this.selectedClient = valorSelecionado;
   }
 }
--- src/app/components/new-rent/new-rent.module.ts
+++ src/app/components/new-rent/new-rent.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [],
 })
-export class NewRentModule { }
+export class NewRentModule {}
